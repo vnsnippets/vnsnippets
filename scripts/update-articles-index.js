9c28229fa31fd6d11fda3df903d81f079c81d07e
Vidush H. Namah <vn.developer@outlook.com>
2021-05-01 02:01:54 +0400

    use fetch (vs node https) for http calls

--- scripts/update-articles-index.js
+++ scripts/update-articles-index.js
@@ -1,52 +1,39 @@
-const fs = require('fs');
-const https = require('https');
+import { writeFile } from 'fs';
 
-const USERNAME = 'vnsnippets';
-const INDEXFILE = 'metadata.json';
+const ARTICLES_PATH = 'data/articles';
+const INDEX_FILE_PATH = 'data/articles/metadata.json';
 const URL = (path) => `https://api.github.com/repos/vnsnippets/vnsnippets/contents/${path}`;
 
 var options = {
-    headers: {'user-agent': 'node.js'}
+   headers: { 'user-agent': 'node.js' }
 };
 
 const metadata = [];
-const HandleArticleDirectory = (e, index) => {
-  if (!e || !e.name || e.name === INDEXFILE) return;
-  console.log("Directory: " + e.name);
-  
-  https.get(URL(e.path), options, (response) => {
-    response.setEncoding('utf8');
-    let body = "";
-    response.on('data', (data) => body += data);
-    
-    response.on('end', () => {
-      body = JSON.parse(body);
-      body.forEach((item) => metadata.push(item.name));
-      
-      console.log(`Directory: ${e.name} (${metadata.length})`);
-    });
-  });
+
+const HandleArticleDirectory = (e) => {
+   if (!e || !e.path || e.path === INDEX_FILE_PATH) return;
+
+   fetch.get(URL(e.path), options)
+      .then((response) => {
+         const data = response.json();
+         data.forEach((item) => metadata.push(item.name))
+
+         console.log(`Directory - ${item.name} :: ${data.length}`)
+      });
 }
 
 const Execute = () => {
-  const path = 'data/articles';
-  
-  https.get(URL(path), options, (response) => {
-    response.setEncoding('utf8');
-    
-    let body = "";
-    response.on('data', (data) => body += data);
-    
-    response.on('end', () => {
-      body = JSON.parse(body);
-      body.forEach(HandleArticleDirectory);
-      
-      fs.writeFile('data/metadata.json', metadata, 'utf-8', (err) => {
-        if (err) throw err;
-        console.log('Metadata updated');
+   fetch.get(URL(ARTICLES_PATH), options)
+      .then((response) => {
+         const data = response.json();
+
+         data.forEach(HandleArticleDirectory);
+         console.log("Metadata update complete.");
       });
-    });
-  });
+
+   writeFile(INDEX_FILE_PATH, JSON.stringify(metadata), 'utf-8', (err) => {
+      if (err) throw err;
+   });
 }
-  
+
 Execute();
