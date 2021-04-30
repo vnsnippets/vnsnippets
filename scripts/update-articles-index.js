9c655200b705c56f5b610df46f9f412e54448ff7
Vidush H. Namah <vn.developer@outlook.com>
2021-05-01 01:29:42 +0400

    Create update-articles-index.js

---
+++ scripts/update-articles-index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const https = require('https');
+
+const INDEXFILE = 'metadata.json';
+const URL = (path) => `https://api.github.com/repos/vnsnippets/vnsnippets/contents/${path}`;
+
+const metadata = [];
+const HandleArticleDirectory = (e, index) => {
+  if (!e || !e.name || e.name === INDEXFILE) return;
+  console.log("Directory: " + e.name);
+  
+  https.get(URL(e.path), (response) => {
+    response.setEncoding('utf8');
+    let body = "";
+    response.on('data', (data) => body += data);
+    
+    response.on('end', () => {
+      body = JSON.parse(body);
+      body.forEach((item) => metadata.push(item.name));
+      
+      console.log(`Directory: ${e.name} (${body.length})`);
+    });
+  });
+}
+
+const Execute = () => {
+  const path = 'data/articles';
+  
+  https.get(URL(path), (response) => {
+    response.setEncoding('utf8');
+    
+    let body = "";
+    response.on('data', (data) => body += data);
+    
+    response.on('end', () => {
+      body = JSON.parse(body);
+      body.forEach(HandleArticleDirectory);
+      
+      fs.writeFile('data/metadata.json', JSON.stringify(metadata), 'utf-8', (err) => {
+        if (err) throw err;
+        console.log('Metadata updated');
+      });
+    });
+  });
+}
+  
+Execute();
