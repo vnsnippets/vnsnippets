0d1728f8bbef76f1cd8b79065f2a174e1099a593
Vidush H. Namah <vn.developer@outlook.com>
2021-05-06 23:59:24 +0400

    update layout

--- code/layouts/default.vue
+++ code/layouts/default.vue
@@ -1,20 +1,28 @@
 <template>
   <div class="bg-gray-800 min-w-full min-h-screen">
-    <navigation-bar />
+    <branding />
     <nuxt />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import NavigationBar from '../components/NavigationBar.vue';
+import Branding from '../components/Branding.vue';
+
 export default Vue.extend({
-  components: { NavigationBar }
+  components: { Branding }
 })
 </script>
 
 <style>
 @import url('https://fonts.googleapis.com/css?family=Merriweather&display=swap');
+
+@font-face {
+  font-family: 'CaptainIcon';
+  font-style: normal;
+  font-weight: normal;
+  src: url("~assets/fonts/captain-icon.ttf") format("truetype")
+}
 
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
