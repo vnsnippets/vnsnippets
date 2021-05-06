684e5f65ae75b68112a6fc4a8effcd0046bd0e69
Vidush H. Namah <vn.developer@outlook.com>
2021-05-06 23:58:55 +0400

    Replace navigation bar with branding component

---
+++ code/components/Branding.vue
@@ -0,0 +1,38 @@
+<template>
+<nav>
+   <div class="max-w-7xl mx-auto p-4 pb-5">
+      <div class="flex items-center justify-end pb-4">
+         <navigation />
+         <social-media theme="dark" />
+      </div>
+
+      <div class="relative items-center">
+         <div class="brand flex">
+            <router-link class="mx-auto" to="/">
+              <span class="text-gray-600">VN</span>
+              <span class="text-gray-600"> | </span>
+              <span class="text-gray-200"><strong>SNIPPETS</strong></span>
+            </router-link>
+          </div>
+      </div>
+   </div>
+</nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Navigation from './Navigation.vue';
+import SocialMedia from './SocialMedia.vue';
+
+export default Vue.extend({
+   components: { Navigation, SocialMedia }
+})
+</script>
+
+<style scoped>
+.brand {
+   font-family: 'Merriweather', sans-serif;
+   font-weight: 100;
+   font-size: 24px;
+}
+</style>
