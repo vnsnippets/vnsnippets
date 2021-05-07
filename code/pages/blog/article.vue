468b3879ebbc1449552060e569e81f395a6c351d
Vidush H. Namah <vn.developer@outlook.com>
2021-05-08 02:59:19 +0400

    basic blog home design

--- code/pages/blog/article.vue
+++ code/pages/blog/article.vue
@@ -1,19 +1,20 @@
 <template>
   <div class="container">
-    You are now viewing Lord Snippet's blog section's default layout
+    You are now viewing Lord Snippet's blog section's article
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  layout: 'blog'
+})
 </script>
 
 <style>
 .container {
   margin: 0 auto;
-  min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
