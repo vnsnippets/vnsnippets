87c881cd4ac776bb208bb28029fe62591b746ab3
Vidush H. Namah <vn.developer@outlook.com>
2021-05-08 02:57:51 +0400

    update configs and dependencies

--- code/nuxt.config.js
+++ code/nuxt.config.js
@@ -23,7 +23,8 @@ export default {
   ** Global CSS
   */
   css: [
-    '~/assets/fonts/captain-icon.css'
+    '~/assets/fonts/captain-icon.css',
+    '~/assets/css/styles.css'
   ],
   /*
   ** Plugins to load before mounting the App
