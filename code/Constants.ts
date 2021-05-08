2ceee76c9a8e4989d83ff36aad3b6c86460fd042
Vidush H. Namah <vn.developer@outlook.com>
2021-05-09 00:46:23 +0400

    shared constants and mock data

---
+++ code/Constants.ts
@@ -0,0 +1,3 @@
+export const BackgroundURL = (url: string) => `url(${url})`
+export const DEFAULT_IMG =
+  'https://images.unsplash.com/photo-1536623975707-c4b3b2af565d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
